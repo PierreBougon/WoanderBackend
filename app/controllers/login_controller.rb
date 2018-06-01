6d76379720450f4cd50197a78da0d075f4a7cf69
Marc Brout <marc.brout@epitech.eu>
2018-06-01 20:14:32 +0200

    Start of password reset changes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,22 @@
 class LoginController < ApplicationController
   before_action :authenticate!
+  before_action :consume_single_use_oauth2_token, only: [:resetpassword]
 
   def login
     render json: { response: 'logged' }
+  end
+
+  def onetimetoken
+    render json: { token: current_login.single_use_oauth2_token }
+  end
+
+  def resetpassword
+    user = User.find_by(current_login.user_id)
+    
+    if user && user.reset_password!(params[:password])
+      render json: {status: 'ok'}, status: :ok
+    else
+      render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_one :login
   validates_presence_of :email, :username
   validates_uniqueness_of :email, :username
+
+  def reset_password!(password)
+    self.login.password_digest = password
+    save!
+  end
 end
